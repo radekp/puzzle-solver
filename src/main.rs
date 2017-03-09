65305a1a3b78c448f55ed6f2fcc2d4f0834e7ff4
Radek Polak <psonek2@seznam.cz>
2017-03-09 11:48:56 +0100

    sdl render without saving

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,10 @@ use std::path::Path;
 
 use image::GenericImage;
 
-use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::image::{LoadTexture, INIT_PNG, INIT_JPG};
 
 // Maximal wifth/height for pieces array
 const MAX_WIDTH: usize = 512;
@@ -492,28 +492,47 @@ fn main() {
     let _ = im.save(fout, image::PNG).unwrap();
 
 
-    let sdl_context = sdl2::init().unwrap();
+   let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-    let _image_context = sdl2::image::init(INIT_PNG | INIT_JPG).unwrap();
+
     let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
-      .position_centered()
-      .build()
-      .unwrap();
+        .position_centered()
+        .opengl()
+        .build()
+        .unwrap();
 
-    let mut renderer = window.renderer().software().build().unwrap();
-    let texture = renderer.load_texture("IMG_20170225_152806.jpg.png").unwrap();
+    let mut renderer = window.renderer().build().unwrap();
 
-    renderer.copy(&texture, None, None).expect("Render failed");
+    let mut texture = renderer.create_texture_streaming(
+        PixelFormatEnum::RGB24, MAX_WIDTH as u32, MAX_HEIGHT as u32).unwrap();
+    // Create a red-green gradient
+    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+        for y in 0..MAX_HEIGHT {
+            for x in 0..MAX_WIDTH {
+                let offset = y*pitch + x*3;
+                buffer[offset + 0] = pixels[x][y];
+                buffer[offset + 1] = pixels[x][y];
+                buffer[offset + 2] = pixels[x][y];
+            }
+        }
+    }).unwrap();
+
+    renderer.clear();
+    renderer.copy(&texture, None, Some(Rect::new(0, 0, MAX_WIDTH as u32, MAX_HEIGHT as u32))).unwrap();
     renderer.present();
 
-    'mainloop: loop {
-        for event in sdl_context.event_pump().unwrap().poll_iter() {
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop {
+        for event in event_pump.poll_iter() {
             match event {
-                Event::Quit{..} |
-                Event::KeyDown {keycode: Option::Some(Keycode::Escape), ..} =>
-                    break 'mainloop,
+                Event::Quit {..} 
+                | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
                 _ => {}
             }
         }
+        // The rest of the game loop goes here...
     }
 }
