1510293876d7e7a47f1d2c74d470340c81a096a8
Radek Polak <polak@odp.cz>
2017-06-05 19:20:53 +0200

    nicer format

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,31 @@ use std::io::Read;
 static INDEX: &'static [u8] = br#"
 <html>
 <body>
+<canvas id="canvas" width="500" height="500"></canvas>
 <form action="/action_page_post.php" method="post" enctype="multipart/form-data">
-<input type="file" name="filename" accept="image/gif, image/jpeg, image/png">
+<input type="file" name="filename" accept="image/gif, image/jpeg, image/png" id="uploadimage">
 <input type="submit" value="Submit">
+<script type="text/javascript">//<![CDATA[
+
+function draw(ev) {
+    console.log(ev);
+    var ctx = document.getElementById('canvas').getContext('2d'),
+        img = new Image(),
+        f = document.getElementById("uploadimage").files[0],
+        url = window.URL || window.webkitURL,
+        src = url.createObjectURL(f);
+
+    img.src = src;
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, 640, 480);
+        url.revokeObjectURL(src);
+    }
+}
+
+document.getElementById("uploadimage").addEventListener("change", draw, false)
+//]]>
+
+</script>
 </form>
 </body>
 </html>"#;
